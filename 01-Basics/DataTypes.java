0c150b4ef7fb83a2eb8b8290454aab7702503c79
Mounishwaran s <mounishwarans2@gmail.com>
2026-09-08 14:16:33 +0530

    Add DataTypes class to demonstrate Java data types
    This file demonstrates various Java data types including integer, decimal, character, and boolean types with example values and print statements.

---
+++ 01-Basics/DataTypes.java
@@ -0,0 +1,29 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        // Integer data type
+        byte byteValue = 100;
+        short shortValue = 20000;
+        int intValue = 100000;
+        long longValue = 10000000000L;
+
+        // Decimal data type
+        float floatValue = 10.5f;
+        double doubleValue = 99.99;
+
+        // Character data type
+        char characterValue = 'A';
+
+        // Boolean data type
+        boolean booleanValue = true;
+
+        System.out.println("Byte: " + byteValue);
+        System.out.println("Short: " + shortValue);
+        System.out.println("Int: " + intValue);
+        System.out.println("Long: " + longValue);
+        System.out.println("Float: " + floatValue);
+        System.out.println("Double: " + doubleValue);
+        System.out.println("Character: " + characterValue);
+        System.out.println("Boolean: " + booleanValue);
+    }
+}
