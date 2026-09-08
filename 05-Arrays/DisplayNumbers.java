f26055f2feeb5ef667eee6f3661f0f1f7b53df9c
Mounishwaran s <mounishwarans2@gmail.com>
2026-09-08 14:35:43 +0530

    Add DisplayNumbers class to print numbers 1 to 10

---
+++ 05-Arrays/DisplayNumbers.java
@@ -0,0 +1,12 @@
+public class DisplayNumbers {
+    public static void main(String[] args) {
+
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        System.out.println("Numbers from 1 to 10:");
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
