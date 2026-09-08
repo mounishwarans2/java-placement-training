9bfafeff42ff879259fd0be67ebc9f7d8ccad92d
Mounishwaran s <mounishwarans2@gmail.com>
2026-09-08 14:33:12 +0530

    Create NumberStarPattern.java

---
+++ 04-Loops/04-Loops/NumberStarPattern.java
@@ -0,0 +1,18 @@
+public class NumberStarPattern {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 6; i++) {
+
+            for (int j = 1; j <= i; j++) {
+
+                if (i % 2 == 0) {
+                    System.out.print(i);
+                } else {
+                    System.out.print("*");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
