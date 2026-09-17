049d53957c8865eb2234f6508b8eeda8f6b69e53
Mounishwaran s <mounishwarans2@gmail.com>
2026-09-17 14:35:16 +0530

    Add LinkedList example in LinkedList-Example.java

---
+++ 01-Basics/LinkedList-Example.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+public class main{
+    public static void main(String [] args){
+        LinkedList<Integer>numbers = new LinkedList<>();
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+        numbers.add(40);
+        System.out.println(numbers);
+    }
+}
