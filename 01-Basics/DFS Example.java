5ec43d111ebdbff9b488c536adf96f863730189f
Mounishwaran s <mounishwarans2@gmail.com>
2026-09-21 15:28:34 +0530

    Add DFS example implementation in Java

---
+++ 01-Basics/DFS Example.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Main{
+    static ArrayList<ArrayList<Integer>>graph ;
+    static void dfs (int node, boolean[]visited){
+        visited[node]=true;
+        System.out.print(node + " ");
+        for(int neighbor:graph.get(node)){
+            if(!visited[neighbor]){
+                dfs(neighbor,visited);
+            }
+        }
+    }
+    public static void main(String[] args){
+        int vertices=5;
+        graph=new ArrayList<>();
+        for(int i = 0;i < vertices;i++){
+            graph.add(new ArrayList <>());
+        }
+        graph.get(0).add(1);
+        graph.get(1).add(0);
+        graph.get(0).add(2);
+        graph.get(2).add(0);
+        graph.get(1).add(3);
+        graph.get(3).add(1);
+        graph.get(1).add(4);
+        graph.get(4).add(1);
+        boolean[] visited = new boolean[vertices];
+        System.out.print("DFS Traversal.");
+        dfs(0,visited);
+    }
+}
