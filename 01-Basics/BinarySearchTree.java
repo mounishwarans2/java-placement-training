f1adbae533c63412a33ca1b24c3d30da54e4e26d
Mounishwaran s <mounishwarans2@gmail.com>
2026-09-21 16:17:20 +0530

    Create BinarySearchTree.java

---
+++ 01-Basics/BinarySearchTree.java
@@ -0,0 +1,61 @@
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data=data;
+        left=null;
+        right= null;
+    }
+}
+public class BinaryTree{
+static Node insert(Node root,int value){
+    if(root==null){
+        return new Node(value);
+    }
+    if(value<root.data){
+        root.left=insert(root.left,value);
+        }else{
+        root.right=insert(root.right,value);
+}
+return root;
+}
+static boolean search(Node root,int value){
+    if(root==null){
+        return false;
+    }
+    if(root.data==value){
+        return true;
+    }
+    if(value<root.data){
+        return search(root.left,value);
+    }
+    return search(root.right, value);
+}
+static void inorder(Node root){
+    if(root==null){
+    return;    
+    }
+    inorder(root.left);
+    System.out.print(root.data +" ");
+    inorder(root.right);
+}
+public static void main(String[] args){
+    Node root=null;
+    root=insert(root,50);
+    root=insert(root,30);
+    root=insert(root,70);
+    root=insert(root,20);
+    root=insert(root,40);
+    root=insert(root,60);
+    root=insert(root,80);
+    System.out.println("inorder");
+    inorder(root);
+    int value=60;
+    if(search(root,value)){
+        System.out.println("\n"+value+"found");
+    }else{
+        System.out.println("\n"+value+"not found");    
+}
+}
+}
